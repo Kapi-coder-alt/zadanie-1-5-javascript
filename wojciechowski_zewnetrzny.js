8671c42499c4e7f31ba9ce0462bc5542bbd12fd3
Kapi <89089481+Kapi-coder-alt@users.noreply.github.com>
2026-09-24 14:32:53 +0200

    Add files via upload

---
+++ wojciechowski_zewnetrzny.js
@@ -0,0 +1,8 @@
+
+document.write("<div style='border:1px solid black; font-family:\"Courier New\",monospace; font-size:14px; padding:2px;'>");
+document.write("Mój numer w dzienniku to 27<br>");
+document.write("Pierwszy wiersz to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2&nbsp;&nbsp;Nie&nbsp;&nbsp;Tak&nbsp;&nbsp;6<br>");
+document.write("Drugi wiersz to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;16&nbsp;&nbsp;Tak&nbsp;&nbsp;Nie&nbsp;&nbsp;6");
+document.write("</div>");
+document.write("ostatnia modyfikacja strony".strike().fontcolor("#C0C0C0").fontsize(6) + "<br>");
+document.write(document.lastModified.bold().fontcolor("#00FFFF").fontsize(6));
